8566fe5c92667df7ed647c9c079ca455d5ba35b8
Renope <r.nouripoor@gmail.com>
2022-05-16 16:59:59 +0430

    ownable removed

--- contracts/PublicNFT.sol
+++ contracts/PublicNFT.sol
@@ -23,10 +23,9 @@ pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract PublicNFT is ERC721, ERC721URIStorage, Ownable {
+contract PublicNFT is ERC721, ERC721URIStorage {
     using Counters for Counters.Counter;
 
     Counters.Counter private _tokenIdCounter;
